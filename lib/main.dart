5ea9393b1c6f49f3b278da65f63394be3c294e75
astutz96 <astutz96.development@protonmail.com>
2020-10-23 15:46:03 -0400

    added AppBar with icons/labels

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,44 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: Text('Layouts in Flutter'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Special(
+
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Special extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Icon(Icons.call),
+              Text('Call'),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Icon(Icons.arrow_back),
+              Text('Route'),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Icon(Icons.share),
+              Text('share'),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
