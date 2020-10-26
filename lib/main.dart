319bce482634440a275ab382379f0f977aaf7c40
astutz96 <astutz96.development@protonmail.com>
2020-10-26 14:42:15 -0400

    adding ButtonSection

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
+            ButtonSection(),
             TitleSection(),
           ],
         ),
@@ -24,6 +25,48 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class ButtonSection extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    Color color = Theme.of(context).primaryColor;
+
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, "CALL"),
+          _buildButtonColumn(color, Icons.near_me, "ROUTE"),
+          _buildButtonColumn(color, Icons.share, "SHARE"),
+        ],
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label){
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            )
+          ),
+        )
+      ],
+    );
+  }
+}
+
 
 class TitleSection extends StatelessWidget {
   @override
