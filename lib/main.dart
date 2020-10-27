19d09d84777750d416f6e7a6e9f3f454107ec8b9
astutz96 <astutz96.development@protonmail.com>
2020-10-26 20:23:40 -0400

    adding Title Section

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,34 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            ButtonSection(),
             TitleSection(),
+            ButtonSection(),
+            TextSection(),
           ],
         ),
       ),
     );
   }
 }
+
+class TextSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+  }
+}
+
 
 class ButtonSection extends StatelessWidget {
 
@@ -67,7 +87,6 @@ class ButtonSection extends StatelessWidget {
   }
 }
 
-//asdf
 class TitleSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
